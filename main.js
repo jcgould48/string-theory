a90bf6cea70d0e42725e15c358556fee58302340
Jesse Gould <jesse.gould@codeimmersives.com>
2019-10-21 22:45:32 -0400

    green lights

--- main.js
+++ main.js
@@ -1,26 +1,134 @@
-function crazyCase() {
+function crazyCase(str) {
+  let newStr = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (i % 2 === 1) {
+      
+      newStr = newStr + str[i].toUpperCase();
+    } else {
+      newStr = newStr + str[i].toLowerCase();
+    }
+  }
+    return newStr
+  }
+
+function ciEmailify(str) {
+  let newStr = '';
+ 
+  for (let i = 0; i < str.length; i++){
+    
+      if (str[i] === ' ') {
+        
+        newStr = newStr + '.';
+      } else {
+        newStr = newStr + str[i].toLowerCase();
+      }
+  
+    }
+      return newStr +'@codeimmersives.com'    
+  }
+
+
+function exclaim(str) {
+let newStr = '';
+for (let i = 0; i < str.length; i++){
+  
+    if (str[i] === '?' || str[i] === '.') {
+      
+      newStr = newStr + '!';
+    } else {
+      newStr = newStr + str[i];
+    }
+
+  }
+
+  return newStr
 }
 
-function ciEmailify() {
+
+function reverse(str) {
+let newStr = '';
+for(let i = str.length - 1; i >= 0; i --){
+newStr = newStr + str[i]
+}
+return newStr;
 }
 
-function exclaim() {
+function crazyCase2ReturnOfCrazyCase(str) {
+  let newStr = '';
+  let count = 0
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ' '){
+    newStr = newStr + str[i].toLowerCase();
+    }
+    else if (count % 2 === 1) {
+      
+      newStr = newStr + str[i].toUpperCase();
+      count ++;
+    } else {
+      newStr = newStr + str[i].toLowerCase();
+      count ++
+    }
+  }
+    return newStr
+  }
+
+
+function titleCase(str) {
+
+  let newStr = '';
+  let tracker = 0
+  for (let i = 0; i < str.length; i++){
+if (i === 0 || tracker === 1){
+  
+  newStr = newStr + str[i].toUpperCase();
+  tracker = 0;
+}
+else if(str[i] === ' '){
+  newStr = newStr + str[i].toLowerCase();
+  tracker ++;
+}
+else {
+  newStr = newStr + str[i].toLowerCase();
+  tracker = 0;
+}
+}
+return newStr;
 }
 
-function reverse() {
+function onlyVowels(str) {
+  let newStr = '';
+  let vowel = 'aeiouAEIOU'
+  for(let i = 0; i < str.length; i++){
+    if (vowel.includes(str[i])){
+      newStr = newStr + str[i];
+    }
+  }
+  return newStr
 }
+//arr[1] = arr[1] % 2 === 1 ? 'odd' : 'even'
 
-function crazyCase2ReturnOfCrazyCase() {
-}
+function crazyCase3SonOfCrazyCase(str) {
+  let newStr = '';
+  let crazies = ' 1234567890!@#$%.,?/()'
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
 
-function titleCase() {
-}
+    if (crazies.includes(str[i])){
+    newStr = newStr + str[i].toLowerCase();
+    }
+    else if (count % 2 === 1) {
+      
+      newStr = newStr + str[i].toUpperCase();
+      count ++;
+    } else {
+      newStr = newStr + str[i].toLowerCase();
+      count ++
+    }
+  }
+    return newStr
+  }
 
-function onlyVowels() {
-}
-
-function crazyCase3SonOfCrazyCase() {
-}
 
 
 module.exports = {
